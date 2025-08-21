c3e08b1003e2ba524ca8295f2964175237e2475f
Thiago Rodrigues <thiagoneye@outlook.com>
2025-08-21 17:38:03 -0300

    Update Repository

--- 01. Bancos de Dados Relacionais e Linguagem SQL/Unidade 02/query06.sql
+++ 01. Bancos de Dados Relacionais e Linguagem SQL/Unidade 02/query06.sql
@@ -4,6 +4,21 @@ SELECT nom_empregado AS Nome, nom_cidade + '-' + sig_uf AS nom_cidade
 
 
 
+SELECT 'x' + (' ' + nom_depto + ' ') + 'x' AS com_espacos,
+    'x' + RTRIM(' ' + nom_depto + ' ') + 'x' AS com_rtrim,
+    'x' + LTRIM(' ' + nom_depto + ' ') + 'x' AS com_ltrim,
+    'x' + TRIM(' ' + nom_depto + ' ') + 'x' AS com_trim
+        FROM departamento
+            WHERE nom_depto = 'Compras';
+
+
+
+SELECT nom_depto,
+    UPPER(nom_depto) AS upper_name,
+    LOWER(nom_depto) AS lower_name
+        FROM departamento;
+
+
 
 
 
