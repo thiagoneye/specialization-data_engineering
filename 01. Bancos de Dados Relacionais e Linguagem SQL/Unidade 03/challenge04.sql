dc58a1074290bfea840a51c9182a200a49b8bc9e
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2025-11-02 22:23:42 -0300

    Add SQL queries for film genre filtering
    Added SQL queries to list films by genre criteria.

--- 01. Bancos de Dados Relacionais e Linguagem SQL/Unidade 03/challenge04.sql
+++ 01. Bancos de Dados Relacionais e Linguagem SQL/Unidade 03/challenge04.sql
@@ -1,1 +1,77 @@
+-- Consulta 01. Listar os filmes que são do gênero Guerra ou do Gênero Ação.
 
+-- Opção 1
+SELECT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero = 'Guerra'
+UNION
+SELECT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero = 'Ação';
+
+-- Opção 2
+SELECT DISTINCT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero IN ('Guerra', 'Ação');
+
+-- Consulta 02. Listar os filmes que são do gênero Guerra e também do Gênero Ação
+
+-- Opção 1
+SELECT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero = 'Guerra'
+INTERSECT
+SELECT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero = 'Ação';
+
+-- Opção 2
+SELECT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero = 'Guerra'
+AND dsc_filme IN 
+     (SELECT dsc_filme
+     FROM filmes f
+     JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+     JOIN generos g ON g.id_genero = fg.id_genero
+     WHERE g.dsc_genero = 'Ação');
+
+-- Consulta 03. Listar os filmes que são do gênero Guerra e não são do Gênero Ação
+
+-- Opção 1
+SELECT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero = 'Guerra'
+EXCEPT
+SELECT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero = 'Ação';
+
+-- Opção 2
+SELECT dsc_filme
+FROM filmes f
+JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+JOIN generos g ON g.id_genero = fg.id_genero
+WHERE g.dsc_genero = 'Guerra'
+AND dsc_filme NOT IN 
+     (SELECT dsc_filme
+     FROM filmes f
+     JOIN filmes_genero fg ON fg.id_filme = f.id_filme
+     JOIN generos g ON g.id_genero = fg.id_genero
+     WHERE g.dsc_genero = 'Ação');
